fdb71d5b048386122494820d317b340470e8bedc
mohithaa-p <122714462+mohithaa-p@users.noreply.github.com>
2023-12-22 22:44:11 +0530

    footer.js

---
+++ alumni-application-frontend/src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={styles.footer}>
+    </footer>
+  );
+};
+
+const styles = {
+  footer: {
+    backgroundColor: '#0F5CBF',
+    padding: '10px',
+    position: 'fixed',
+    bottom: '0',
+    width: '100%',
+    height: '165px',
+    flexShrink: 0,
+  },
+  
+};
+
+export default Footer;
